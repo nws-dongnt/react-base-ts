80222d8fb0bf3a34e4beb0721552be7af50d46c8
dongnt <dongnt@newwave.vn>
2023-07-24 17:39:35 +0700

    react-i18n base

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from "react";
-import "./App.css";
-import Router from "./route/Router";
+import "App.css";
+import Router from "route/Router";
+import { I18nextProvider } from "react-i18next";
+import i18n from "i18n";
 
 function App() {
   return (
     <div className="App">
-      <Router />
+      <I18nextProvider i18n={i18n}>
+        <Router />
+      </I18nextProvider>
     </div>
   );
 }
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { ChangeEvent } from "react";
 import { Outlet, Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import i18n from "i18n";
 import BaseLayout from "./BaseLayout";
 
 export default function MainLayout() {
+  const { t } = useTranslation();
+  const { t: tNavBar } = useTranslation("translation", { keyPrefix: "navBar" });
+
+  const handleChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    i18n.changeLanguage(event.target.value);
+  };
+
   return (
     <BaseLayout>
       <div
@@ -10,14 +19,22 @@ export default function MainLayout() {
           display: "flex",
           gap: 15,
           justifyContent: "center",
+          alignItems: "center",
           backgroundColor: "#282c34",
         }}
       >
+        <p>{t("navBar.chooseLanguage")}</p>
+        <div>
+          <select onChange={handleChange}>
+            <option value="vi">{t("navBar.vi")}</option>
+            <option value="en">{t("navBar.en")}</option>
+          </select>
+        </div>
         <Link to="/">
-          <h1>Home</h1>
+          <h1>{tNavBar("home")}</h1>
         </Link>
         <Link to="/about">
-          <h1>About</h1>
+          <h1>{tNavBar("about")}</h1>
         </Link>
       </div>
       <Outlet />
